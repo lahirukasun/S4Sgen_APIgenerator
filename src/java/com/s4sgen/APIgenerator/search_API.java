fb4ddac96a38c86e724eb27a54b1076c40e46df7
lahirukasun <web.lahiru@gmail.com>
2017-08-22 11:51:54 +0530

    Initial commit

---
+++ src/java/com/s4sgen/APIgenerator/search_API.java
@@ -0,0 +1,32 @@
+package com.s4sgen.APIgenerator;
+
+import java.io.File;
+
+public class search_API {
+
+    public static void main(String[] args) {
+
+        searchAPI("s4sgen_apiID_edu_SMS0001");
+
+    }
+
+    public static void searchAPI(String sname) {
+        
+        
+        File file = new File("G:\\S4Sgen\\system_resource\\");
+        String[] names = file.list();
+
+        for (String name : names) {
+            if (new File("G:\\S4Sgen\\system_resource\\" + sname).isDirectory()) {
+                System.out.println("Search result : "+sname);
+                
+                break;
+            }
+            else{
+                System.out.println("no such a file or derectory");
+                break;
+            }
+        }
+    }
+
+}
